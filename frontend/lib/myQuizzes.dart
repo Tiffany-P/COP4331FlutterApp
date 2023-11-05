f166d88844ec6e12321ba1337a43ccc3db11a016
Tiffany-P <91702564+Tiffany-P@users.noreply.github.com>
2023-11-05 10:33:08 -0500

    creating my quizzes page

---
+++ frontend/lib/myQuizzes.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'register.dart';
+
+class myQuizzesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QuizWizz'),
+        backgroundColor: Color.fromARGB(255, 86, 17, 183),
+        leading: GestureDetector(
+          onTap: () {/* Write listener code here */},
+          child: Icon(
+            Icons.menu, // add custom icons also
+          ),
+        ),
+        actions: <Widget>[
+          Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {},
+                child: Icon(
+                  Icons.search,
+                  size: 26.0,
+                ),
+              )),
+          Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {},
+                child: Icon(Icons.more_vert),
+              )),
+        ],
+      ),
+      backgroundColor: Color.fromARGB(255, 41, 5, 73),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const SizedBox(height: 30.0),
+          Text(
+            '   Popular',
+            style: TextStyle(
+              color: Colors.white, // Set text color to white
+              fontSize: 23, // Set text font size
+            ),
+          ),
+          const SizedBox(height: 30.0),
+          Text(
+            '   Suggested',
+            style: TextStyle(
+              color: Colors.white, // Set text color to white
+              fontSize: 23, // Set text font size
+            ),
+          ),
+          const SizedBox(height: 30.0),
+        ],
+      ),
+    );
+  }
+}
---
+++ server.js
@@ -0,0 +1,55 @@
+// Import libraries
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production')
+{
+  // Set static folder
+  app.use(express.static('frontend/build'));
+  app.get('*', (req, res) =>
+  {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
+app.set('port', (process.env.PORT || 5000));
+app.use((req, res, next) =>
+{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+  'Access-Control-Allow-Headers',
+  'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+  'Access-Control-Allow-Methods',
+  'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
+
+// Use middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Import api endpoints
+const usersRouter = require("./api/users");
+const quizzesRouter = require("./api/quizzes");
+const questionsRouter = require("./api/questions");
+const savedRouter = require("./api/saved");
+
+// Use routes
+app.use("/api/users", usersRouter);
+app.use("/api/quizzes", quizzesRouter);
+app.use("/api/questions", questionsRouter);
+app.use("/api/saved", savedRouter);
+
+// Start server
+app.listen(PORT, () => {
+  console.log("Server listening on port " + PORT);
+});
